935e08bf104e308706ecb74c531da2bef17e0c7c
YaoHong <Yao_h@sina.cn>
2020-01-01 23:35:52 +0800

    素材管理完成

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
 import axios from '@/api'
+import '@/styles/index.less'
 // 使用自己的插件
 import plugin from '@/plugin'
 Vue.use(plugin)
