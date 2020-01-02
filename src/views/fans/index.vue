6c3d75ee924e9d07a68c0726083b941182177984
YaoHong <Yao_h@sina.cn>
2020-01-02 21:46:06 +0800

    粉丝管理完成

--- src/views/fans/index.vue
+++ src/views/fans/index.vue
@@ -4,14 +4,133 @@
       <div slot="header">
         <my-bread>粉丝管理</my-bread>
       </div>
+      <el-tabs v-model="activeName" type="card">
+        <el-tab-pane label="粉丝列表" name="list">
+          <div class="fans_list">
+            <div class="fans_item" v-for="item in fanslist" :key="item.id.toString()">
+              <!-- 头像 -->
+              <el-avatar :size="80" :src="item.photo"></el-avatar>
+              <!-- 用户名 -->
+              <p>{{item.name}}</p>
+              <!-- 关注按钮 -->
+              <el-button type="primary" plain size="small">+关注</el-button>
+            </div>
+          </div>
+          <el-pagination
+            style="margin-top:20px"
+            background
+            layout="prev, pager, next"
+            :total="total"
+            :current-page="reqParams.page"
+            :page-size="reqParams.per_page"
+            @current-change="changePager"
+          ></el-pagination>
+        </el-tab-pane>
+        <el-tab-pane label="粉丝画像" name="picture">
+          <div ref="dom" style="width: 600px;height:400px;"></div>
+        </el-tab-pane>
+      </el-tabs>
     </el-card>
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
 export default {
+  data () {
+    return {
+      // 粉丝列表
+      fanslist: [],
+      // 请求参数
+      reqParams: {
+        page: 1,
+        per_page: 21
+      },
+      // 总条数
+      total: 0,
+      // 当前激活的选择卡名称
+      activeName: 'list'
+    }
+  },
+  mounted () {
+    // 初始化：
+    const myChart = echarts.init(this.$refs.dom)
+    // 准备一个配置对象（约定图表怎么绘制）, 请参考官方提供示例。
+    const option = {
+      color: ['#3398DB'],
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          // 坐标轴指示器，坐标轴触发有效
+          type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+        }
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: '直接访问',
+          type: 'bar',
+          barWidth: '60%',
+          data: [10, 52, 200, 334, 390, 330, 220]
+        }
+      ]
+    }
+    // 使用配置：
+    myChart.setOption(option)
+  },
+  created () {
+    this.getFansList()
+  },
+  methods: {
+    // 分页
+    changePager (newPage) {
+      this.reqParams.page = newPage
+      this.getFansList()
+    },
+    // 获取粉丝列表数据
+    async getFansList () {
+      const {
+        data: { data }
+      } = await this.$http.get('followers', { params: this.reqParams })
+      this.fanslist = data.results
+      this.total = data.total_count
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.fans_item {
+  width: 120px;
+  height: 170px;
+  border: 1px dashed #ddd;
+  text-align: center;
+  padding: 10px 0;
+  display: inline-block;
+  margin-right: 30px;
+  margin-bottom: 15px;
+  p {
+    font-size: 12px;
+    padding: 5px 0;
+  }
+}
 </style>
